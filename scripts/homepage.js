e107e037f180239cc2e8b0f206ba4c44c430aab2
Travis Wight <tman.wight@gmail.com>
2020-02-13 12:09:17 -0800

    Fixed About Image Gallery
    Made the about image gallery similar responsiveness as portfolio section cards.

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,17 +1,30 @@
 $(document).ready(function(){
 	// Portfolio section:
 
-	$(window).resize(function(){
-		var width = $(".portfolio-col-cont-main").width();
+	function setFrame(){
+		var about_img_width = $(".sect-short-img-cont-main").width();
+		var port_width = $(".portfolio-col-cont-main").width();
+
+		// For About Gallery:
+		
+		$(".sect-short-img-cont-main").css("height", about_img_width + "px");
+
+		// For Portfolio Cards
 
 		if($(window).width() <= 783){
-			$(".portfolio-col-cont-main").css("height", width + "px");
+			$(".portfolio-col-cont-main").css("height", port_width + "px");
 		}
 		else if($(window).width() <= 1583){
-			$(".portfolio-col-cont-main").css("height", (width)*2 + "px");
+			$(".portfolio-col-cont-main").css("height", (port_width)*2 + "px");
 		}
 		else{
 			$(".portfolio-col-cont-main").css("height", "800px");
 		}
+	}
+
+	setFrame();
+
+	$(window).resize(function(){
+		setFrame();
 	});
 });
