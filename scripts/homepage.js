08548e7bb4e35e21f7ba61fc3cd4d280e51335a0
Travis Wight <tman.wight@gmail.com>
2020-03-03 14:17:14 -0800

    Contact Section
    Layout mostly done. Slight spacing and padding issues, but I'll take care of them tomorrow.

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){
-	// Portfolio section:
-
 	function setFrame(){
 		var about_img_width = $(".sect-short-img-cont-main").width();
 		var port_width = $(".portfolio-col-cont-main").width();
-
+		var contact_form_width = $("#contact-form-cont").width();
 		// For About Gallery:
 		
 		$(".sect-short-img-cont-main").css("height", about_img_width + "px");
 
-		// For Portfolio Cards
+		// For Portfolio Cards:
 		
 		if(($(window).width() <= 1583) && ($(window).width() >= 684)){
 			$(".portfolio-col-cont-main").css("height", (port_width)*2 + "px");
@@ -21,22 +19,31 @@ $(document).ready(function(){
 
 	function setTrianDiv(){
 		var sect_divs = $(".sect-div");
+		var top_heights = {
+			"main-sect-about-sect-div": ($("#about-descriptions").height() + 100) + "px",
+			"main-sect-portfolio-sect-div": ($("#portfolio-short").height()/2) + "px",
+			"main-sect-contact-sect-div": ($("#contact-form-cont").height()/2) + "px",
+		}
 
 		if(sect_divs.length > 0){
 			for(var i = 0; i < sect_divs.length; i++){
-				$(sect_divs[i]).css("border-left-width", $(window).width()/2 + "px");
-				$(sect_divs[i]).css("border-right-width", $(window).width()/2 + "px");
-				$(sect_divs[i]).css("top", $(sect_divs[i]).prev().offset().top + $(sect_divs[i]).prev().height() + "px");
-				$(sect_divs[i]).css("border-top-color", $(sect_divs[i]).prev().css("background-color"));
+				var width = $(sect_divs[i]).width();
+
+				if(top_heights[$(sect_divs[i]).attr("id")] != undefined){
+					$("#" + $(sect_divs[i]).attr("id") + " .sect-div-top").css("height", top_heights[$(sect_divs[i]).attr("id")])
+				}
+
+				$(".sect-div-angle").css("border-right-width", width / 2 + "px");
+				$(".sect-div-angle").css("border-left-width", width / 2 + "px");
 			}
 		}
 	} 
 	
-	setTrianDiv();
 	setFrame();
+	setTrianDiv();
 
 	$(window).resize(function(){
-		setTrianDiv();
 		setFrame();
+		setTrianDiv();
 	});
 });
