ad116ceec56997a201fceb862ea565fd964b2b8a
Travis Wight <tman.wight@gmail.com>
2020-03-06 12:29:24 -0800

    started working on database

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-
+//For testing, change https to http. When production, change back to https. 
 $root = "http://".$_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
---
+++ sql/Create_Tables.sql
@@ -0,0 +1,8 @@
+create table Job(
+	jobID int, 
+    jobType varchar(11),
+    address varchar(255),
+    fullDescription varchar(420),
+    yearCompleted year,
+    
+);
